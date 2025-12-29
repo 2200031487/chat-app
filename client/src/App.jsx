42f9fb8bf32fe762cb22de7d0e299d7269d74f24
2200031487 <2200031487@kluniversity.in>
2025-12-29 09:43:10 +0530

    client is updated for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,23 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast"
 import { AuthContext } from '../context/AuthContext'
+import bgImage from './assets/bgImage.svg'
 
 const App = () => {
-  const {authUser}=useContext(AuthContext)
+  const { authUser } = useContext(AuthContext)
+
   return (
-    <div className="bg-[url('/bgImage.svg')] bg-contain">
-      <Toaster/>
+    <div
+      className="min-h-screen bg-no-repeat bg-center bg-cover"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <Toaster />
       <Routes>
-        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}/>
-        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/"/>}/>
-        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />}/>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
     </div>
   )
